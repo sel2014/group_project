50214a43690d8fb08eb3474a46fb55823e482662
kemal <kemal182182@gmail.com>
2023-03-06 23:25:10 +0000

    kemal commit

--- src/test/java/com/cydeo/step_definitions/Kemal_StepDefinitionsExample.java
+++ src/test/java/com/cydeo/step_definitions/Kemal_StepDefinitionsExample.java
@@ -5,5 +5,6 @@ public class Kemal_StepDefinitionsExample {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
